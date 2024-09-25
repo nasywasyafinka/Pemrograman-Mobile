dc2c5ac93ec7a448d1a4030573bfce3c8a4bd6a3
Nasywa Syafinka Widyamara <syafinkaforcolle@gmail.com>
2024-09-26 00:37:46 +0700

    Pertemuan 2

--- Pertemuan-2/soal1.dart
+++ Pertemuan-2/soal1.dart
@@ -1,5 +1,5 @@
 void main() {
-for (int i =18; i>8; i--){
-  print('Nama saya adalah Nasywa, sekarang berumur ${i}');
+  for (int i = 18; i > 8; i--) {
+    print('Nama saya adalah Nasywa Syafinka, sekarang berumur ${i}');
   }
 }
